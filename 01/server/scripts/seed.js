cab8a732e577ee9dfdd4cfc2f8c80ab4debb6033
fasosnql <nowak_przemek@yahoo.com>
2021-06-12 14:54:35 +0200

    add server

---
+++ 01/server/scripts/seed.js
@@ -0,0 +1,10 @@
+/* @flow */
+/* eslint-disable no-await-in-loop */
+// This script scans `examples` folder for `data/seed.js` files and run them for seeding DB.
+
+import { seedAll } from './seedHelpers';
+
+seedAll().catch((e) => {
+  console.log(e);
+  process.exit(0);
+});
