e6b1d8a1eca214314ab78772d7730bb549309635
fasosnql <nowak_przemek@yahoo.com>
2021-06-26 12:43:06 +0200

    08-finish

--- 08/front/src/pages/ProductDetails/index.js
+++ 08/front/src/pages/ProductDetails/index.js
@@ -1,26 +1,71 @@
 import { useState } from 'react';
 import { PageHeader, Descriptions, Button, Divider } from 'antd';
+import { useParams } from 'react-router-dom';
 import { OrderForm } from './components/OrderForm';
+import { gql, useQuery } from '@apollo/client';
+import { Loader } from '../../components/Loader/Loader';
+ 
+const GET_PRODUCT_DETAILS = gql`
+  query GetProductDetails($productId: Float) {
+    viewer {
+      product(filter: {
+        productID: $productId
+      }) {
+        productID
+        name
+        unitPrice
+        quantityPerUnit
+        category {
+          categoryID
+          name
+        }
+        supplier {
+          companyName
+          contactTitle
+          address {
+            city
+            street
+          }
+        }
+      }
+    }
+  }
+`
 
 function ProductDetails() {
+  const {productId} = useParams();
+  
+  const {data, loading} = useQuery(GET_PRODUCT_DETAILS, {
+    variables: {
+      productId: parseInt(productId)
+    }
+  });
+  
   const [orderFormVisible, setOrderFormVisible] = useState(false);
+  
+  if (loading) {
+    return <Loader />
+  }
+  
+  const {viewer: {product: {name, unitPrice, quantityPerUnit, category, supplier}}} = data;
+  const {contactName, contactTitle, address} = supplier;
 
   return (
     <>
       <PageHeader
         className="site-page-header"
         onBack={() => window.history.back()}
-        title="My product name"
+        title={name}
       />
       <Descriptions title="Product details" bordered>
-        <Descriptions.Item label="Category">something</Descriptions.Item>
-        <Descriptions.Item label="Unit Price">22$</Descriptions.Item>
-        <Descriptions.Item label="Quantity per unit">10 - 10</Descriptions.Item>
+        <Descriptions.Item label="Category">{category.name}</Descriptions.Item>
+        <Descriptions.Item label="Unit Price">{unitPrice}$</Descriptions.Item>
+        <Descriptions.Item label="Quantity per unit">{quantityPerUnit}</Descriptions.Item>
         <Descriptions.Item label="Address">
-          Company name: New Orleans Cajun Delights, <br />
-          Contact title: Order Administrator, <br />
+          Company name: {contactName}, <br />
+          Contact title: {contactTitle}, <br />
           Address: <br />
-          New Orleans, P.O. Box 78934
+          {address.city}, {address.street}
         </Descriptions.Item>
       </Descriptions>
       <Divider />
