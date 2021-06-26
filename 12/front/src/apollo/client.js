3b60664cd672d221bf01603f2136fda31503b780
fasosnql <nowak_przemek@yahoo.com>
2021-06-26 16:18:46 +0200

    12-finish

--- 12/front/src/apollo/client.js
+++ 12/front/src/apollo/client.js
@@ -1,7 +1,32 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:3000/northwind',
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/northwind'
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/northwind',
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
