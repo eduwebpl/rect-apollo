6b17d820f1f35ed2b0e70fdfeb51d229ae300182
fasosnql <nowak_przemek@yahoo.com>
2021-06-27 10:57:47 +0200

    14-finish

--- 14/front/src/pages/Products/index.spec.js
+++ 14/front/src/pages/Products/index.spec.js
@@ -1,50 +1,94 @@
 import {render, waitFor} from '@testing-library/react';
 import {MemoryRouter} from 'react-router-dom';
-import ProductsPage from './index';
-
-jest.mock('@apollo/client', () => ({
-  gql: () => jest.fn(),
-  useQuery: () => ({
-    data: {
-      viewer: {
-        productPagination: {
-            pageInfo: {
-              pageCount: 10,
-              currentPage: 1
-            },
-            items: [
-              {
-                __typename: "Product",
-                _id: "123",
-                productID: 20,
-                name: "Hello World",
-                unitPrice: "123",
-                category: {
-                  __typename: "Category",
-                  categoryID: 2,
-                  name: 'Category 1'
-                }
-              }
-            ] 
-          }
-        }
-      }
-  })
-}));
+import { MockedProvider } from '@apollo/client/testing';
+import ProductsPage, { GET_PRODUCTS } from './index';
 
 describe('ProductList', () => {  
   it ('renders ProductList page', async () => {
     
-    const {getByText} = render(
+    const mocks = [
+      {
+        request: {
+          query: GET_PRODUCTS,
+          variables: {
+            page: 1,
+            perPage: 8,
+          }
+        },
+        result: {
+          data: {
+            viewer: {
+              productPagination: {
+                  pageInfo: {
+                    pageCount: 10,
+                    currentPage: 1
+                  },
+                  items: [
+                    {
+                      __typename: "Product",
+                      _id: "123",
+                      productID: 20,
+                      name: "Hello World",
+                      unitPrice: "123",
+                      category: {
+                        __typename: "Category",
+                        categoryID: 2,
+                        name: 'Category 1'
+                      }
+                    }
+                  ] 
+                }
+              }
+            }
+        },
+      },
+    ]
+    
+    const {getByText, queryByTestId} = render(
       <MemoryRouter initialEntries={["/"]}>
-        <ProductsPage />
+        <MockedProvider mocks={mocks}>
+         <ProductsPage />
+        </MockedProvider>
       </MemoryRouter>
     );
+    
+    expect(queryByTestId('loader')).toBeInTheDocument();
     
     await waitFor(() => {
       expect(getByText('Hello World')).toBeInTheDocument();
       expect(getByText('Category 1')).toBeInTheDocument();
+      expect(queryByTestId('loader')).not.toBeInTheDocument();
     })
   })
+  
+  it ('renders error state', async () => {
+    
+    const mocks = [
+      {
+        request: {
+          query: GET_PRODUCTS,
+          variables: {
+            page: 1,
+            perPage: 8,
+          }
+        },
+        error: new Error('Error!')
+      },
+    ]
+    
+    const {getByText, queryByText} = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <MockedProvider mocks={mocks}>
+         <ProductsPage />
+        </MockedProvider>
+      </MemoryRouter>
+    );
+  
+    expect(queryByText('Retry')).not.toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(getByText('Retry')).toBeInTheDocument();
+    })
+  })  
 })
 
